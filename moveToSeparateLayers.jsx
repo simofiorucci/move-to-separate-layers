3ed65ba59bb159d84ed73a25d043c51d3bc17233
Simone Fiorucci <simone.fiorucc@gmail.com>
2020-01-27 18:57:51 +0100

    Added check on active document and automatic layer renaming.

---
+++ moveToSeparateLayers.jsx
@@ -0,0 +1,28 @@
+
+/*
+
+Illustrator CC script for moving each element of a selection in a separate new layer with the same name of the element itself.
+======
+Usage:
+1. Select at least one item
+2. Run this script
+
+*/
+
+var activeDoc = app.activeDocument
+
+if (activeDoc > 0) { // check if there is an active document to work on
+  var selection = activeDoc.selection
+  
+  if (selection.length > 0) { // check if anything is selected
+    for (var i = 0; i < selection.length; i++) {
+      newLayer = activeDoc.layers.add()
+      newLayer.name = selection[i].name
+      selection[i].move(newLayer, ElementPlacement.INSIDE)
+    }
+  } else {
+      alert('Select at least an object before running this script')
+  }
+} else {
+  alert('Create a new document or select an active document befor running this script')
+}
